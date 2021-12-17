74a6a4627af61a36a5bd44a22650ce47cde622db
Mike Caine <mikey.caine@btinternet.com>
2021-12-17 06:59:34 +0000

    ignore AnotherFailingTest

--- src/test/java/com/mikeycaine/webfluxdemo/AnotherFailingTest.java
+++ src/test/java/com/mikeycaine/webfluxdemo/AnotherFailingTest.java
@@ -1,9 +1,11 @@
 package com.mikeycaine.webfluxdemo;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+@Ignore
 public class AnotherFailingTest {
 
     @Test
